34080a08bf94adda3fe7b2e796fdb39b87c6bcb9
Avinash Bhandwalkar <avinashbhandwalkar86@gmail.com>
2026-02-13 15:23:44 +0530

    Add files via upload

---
+++ avinashpythone exp 1.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 15:20:28 2026
+
+@author: User
+"""
+
+name = input("enter your name:")
+age = input("enter your age:")
+student_class = input ("enter your classs:")
+division = input ("enter your division:")
+roll_no = input ("enter your roll number:")
+batch = input("enter your batch:")
+phone_no = input("enter your phone number:")
+blood_group = input("enter your blood group:")
+address = input("enter your address:")
+college = input("enter your college name:")
+city = input("enter your city:")
+print("\n-----student information-----")
+print("name:",name)
+print("age:",age)
+print("class:", student_class)
+print("division:", division)
+print("roll number",roll_no)
+print("batch:", batch)
+print("phone number:",phone_no)
+print("blood group:",blood_group)
+print("address:",address)
+print("college:",college)
+print("city:",city)
