f33f54c7671717c3953f6efa25667fa4e702bf51
Avinash Bhandwalkar <avinashbhandwalkar86@gmail.com>
2026-05-06 13:14:23 +0530

    Update program no 1.py

--- Experiment no 2/program no 1.py
+++ Experiment no 2/program no 1.py
@@ -1,7 +1,7 @@
 [5:59 pm, 5/5/2026] 🇦 🇷 🇯 🇺 🇳 💎: # Input Leap Year or Not a Leap Year
 Created on Fri Mar 27 21:05:40 2026
 
-@author: Raj Magar
+@author: avinash bhandwalkar
 year = int(input("Enter year:"))
 if (year % 400 == 0) or (year% 4 == 0 and year% 100 != 0):
 print("Leap Year")
@@ -10,10 +10,3 @@ print(" Not a Leap Year")
 [6:00 pm, 5/5/2026] 🇦 🇷 🇯 🇺 🇳 💎: # Input Leap Year or Not a Leap Year
 Created on Fri Mar 27 21:05:40 2026
 
-@author: Raj Magar
-year = int(input("Enter year:"))
-if (year % 400 == 0) or (year% 4 == 0 and year% 100 != 0):
-print("Leap Year")
-else:
-print(" Not a Leap Year")
-[6:01 pm, 5/5/2026] 🇦 🇷 🇯 🇺 🇳 💎: 
